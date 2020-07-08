0ffa4cf3217a9151e9b1c597c133ca8214449fde
Saúl Laínez <saul.alejandro19@gmail.com>
2020-07-08 12:40:36 -0600

    Se agregaron estilos con sass

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+
+const App = () => (
+// eslint-disable-next-line indent
+<div className='App'>
+    <Header />
+  </div>
+);
+
+export default App;
