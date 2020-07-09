a6bccaf110e2a2a813083910232c8ae183c66a2e
Saúl Laínez <saul.alejandro19@gmail.com>
2020-07-09 11:28:42 -0600

    Penultimo commit.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -9,38 +9,51 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-// eslint-disable-next-line indent
-<div className='App'>
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  console.log(videos);
 
-    <Categories title='Originales de Video'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return (
+  // eslint-disable-next-line indent
+  <div className='App'>
+      <Header />
+      <Search />
 
-    <Footer />
-  </div>
-);
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+            <CarouselItem />
+            <CarouselItem />
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Tendencias'>
+        <Carousel>
+          {videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de Video'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
