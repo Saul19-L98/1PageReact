d80bc248db32cc6b5f1b274bd324079add530968
Saúl Laínez <saul.alejandro19@gmail.com>
2020-07-08 21:55:01 -0600

    Cerca del fin, proyecto casi funcional XD

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,12 +2,44 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
 // eslint-disable-next-line indent
 <div className='App'>
     <Header />
+    <Search />
+
+    <Categories title='Mi lista'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Originales de Video'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Footer />
   </div>
 );
 
