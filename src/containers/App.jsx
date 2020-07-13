db5c80976c3b1d71ed389e057d2b8ef8bc136e0e
Saúl Laínez <saul.alejandro19@gmail.com>
2020-07-13 12:37:44 -0600

    Problema resuelto.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,26 +7,22 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
-  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
+  const initialState = useInitialState(API);
 
-  console.log(videos);
-
-  return (
+  return initialState.length === 0 ? <h1>Loading..</h1> : (
   // eslint-disable-next-line indent
   <div className='App'>
       <Header />
       <Search />
 
-      {videos.mylist.length > 0 && (
+      {initialState.mylist.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
             <CarouselItem />
@@ -39,7 +35,7 @@ const App = () => {
 
       <Categories title='Tendencias'>
         <Carousel>
-          {videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
           <CarouselItem />
         </Carousel>
       </Categories>
